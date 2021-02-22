9d1c797ce730e34378edd8ef9c3b4955a2a6cf84
Mullapudi-Jyothirmayi <73999733+Mullapudi-Jyothirmayi@users.noreply.github.com>
2021-02-22 09:58:30 +0530

    Add files via upload

---
+++ servlet_crud/src/servlet_crud/DBUtil.java
@@ -0,0 +1,18 @@
+package servlet_crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	final static String forNameURL="com.mysql.cj.jdbc.Driver";
+	final static String dBURL="jdbc:mysql://localhost:3306/epsql";
+	final static String username="root";
+	final static String password="Mj2410/01";
+	public static Connection DBconnection() throws SQLException, ClassNotFoundException {
+		
+		Class.forName(forNameURL);
+		Connection con=DriverManager.getConnection(dBURL,username,password);
+		return con;	
+	}
+}
